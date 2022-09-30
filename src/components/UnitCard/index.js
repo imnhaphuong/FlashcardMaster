a701a8e926fc589a39e81c702bc6bb828272c9c2
imnhaphuong <phuongptn.236@gmail.com>
2022-09-30 13:45:45 +0700

    complete unitcard ui component

--- src/components/UnitCard/index.js
+++ src/components/UnitCard/index.js
@@ -1,20 +1,30 @@
-import { Text, View, Image } from 'react-native'
-import React, { Component } from 'react'
-import styles from './style'
+import { Text, View, Image } from "react-native";
+import React, { Component } from "react";
+import styles from "./style";
 
-export class UnitCard extends Component {
-  render() {
-    return (
-      <View style={styles.wrapUnitCard}>
-        <Text style={styles.unitName}>Unit 1</Text>
-        <Text style={styles.numberOfCards}>12 thẻ</Text>
+const UnitCard = (props) => {
+  const unitName = props.unitName ? props.unitName : "Bài 2";
+  const numberOfCards = props.numberOfCards
+    ? `${props.numberOfCards} thẻ`
+    : "12 thẻ";
+  // const avatar = props.avatar ? :
+  const username = props.username ? props.username : "user100233";
+
+  return (
+    <View style={styles.wrapUnitCard}>
+      <View style={styles.unitCard}>
+        <Text style={styles.unitName}>{unitName}</Text>
+        <Text style={styles.numberOfCards}>{numberOfCards}</Text>
         <View style={styles.wrapUser}>
-         <Image style={styles.avatar} source={require('../../../assets/images/avt-default.png')} />
-          <Text style={styles.username}>user012453</Text>
+          <Image
+            style={styles.avatar}
+            source={require("../../../assets/images/avt-default.png")}
+          />
+          <Text style={styles.username}>{username}</Text>
         </View>
       </View>
-    )
-  }
-}
+    </View>
+  );
+};
 
-export default UnitCard
+export default UnitCard;
--- src/screens/class_detail/style.js
+++ src/screens/class_detail/style.js
@@ -1,14 +1,18 @@
-import {StyleSheet} from 'react-native'
+import { StyleSheet } from "react-native";
+import colors from "../../../contains/colors";
 
 const styles = StyleSheet.create({
-    titleBar: {
-        
-    },
-    inforArea: {
+  titleBar: {
+    backgroundColor: "yellow",
+  },
+  inforArea: {},
+  wrapUnits: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    paddingHorizontal: 12,
+  },
+});
 
-    },
-    
-
-})
-
-export default styles
+export default styles;
