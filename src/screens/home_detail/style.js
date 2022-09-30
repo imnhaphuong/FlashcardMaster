995799c325cf23329f5a3c2611bde03930257944
quynhnga4200 <86844092+quynhnga4200@users.noreply.github.com>
2022-10-01 03:16:13 +0700

    Edit Home

--- src/screens/home_detail/style.js
+++ src/screens/home_detail/style.js
@@ -1,80 +1,5 @@
 import {StyleSheet} from 'react-native'
 
-// const styles = StyleSheet.create({
-//   boderbottom: {
-//     borderBottomColor: 'grey',
-//     borderBottomWidth: 1,
-//     marginBottom: 30,
-//     marginBottom: 50
-//   },
-//   content: {
-//     flex: 1,
-//     flexDirection: 'row',
-//     marginTop: 60,
-//     marginLeft: 20,
-//     marginRight: 20,
-//     marginBottom: 20,
-//     height: 50,
-//     backgroundColor: '#fff'
-//   },
-//   coin:{
-//     marginTop: 13,
-//     marginLeft: 60,
-//     width: 25,
-//     height:23,
-//   },
-//   price: {
-//     textAlign: 'right',
-//     marginRight: 20,
-//     marginBottom: 23,
-//     marginTop: -5,
-//     fontSize: 17,
-//     marginLeft: 5,
-//     fontWeight: 'bold'
-//   },
-//   landscape: {
-//     width: 210,
-//     height: 40,
-//   },
-  
-//   rectangle: {
-//     width: 335,
-//     height: 82.8,
-//     left: 20,
-//     top: 17,
-//     backgroundColor: '#FAB8C4',
-//     borderRadius: 10,
-//   },
-//   rectanglecontent:{
-//     fontWeight: '500',
-//     fontFamily: 'Work Sans',
-//     fontSize: 16,
-//     justifyContent: 'center',
-//     marginTop: 30,
-//     marginLeft: 20
-//   },
-//   thegirl: {
-//     resizeMode: 'cover',
-//     width: 112,
-//     height: 106,
-//     marginLeft: 212,
-//     marginTop: -6
-//   },
-//   chatvio: {
-//     resizeMode: 'cover',
-//     width: 48,
-//     height: 24,
-//     marginTop: -90,
-//     marginLeft: 210
-//   },
-//   chatyel: {
-//     resizeMode: 'cover',
-//     width: 48,
-//     height: 24,
-//     marginTop: -110,
-//     marginLeft: 282
-//   }
-// });
 const styles = StyleSheet.create({
   boderbottom: {
     borderBottomColor: '#FFFFFF',
@@ -204,11 +129,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: '#F7D6E1'
   },
-  oke:{
-    backgroundColor: '#000000',
-    width: '100%',
-    flexGrow: 1,
-  }
 });
 
 export default styles
