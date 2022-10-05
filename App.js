d4cd6b6acb169f18e64a341a6fac1a9d543e6d05
LinhTrang <trangnguyen24201@gmail.com>
2022-10-05 16:09:11 +0700

    create ui sign in

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ClassDetailScreen from "./src/screens/class_detail";
 import Home from "./src/screens/home_detail/Home_Screen";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SignInScreen from "./src/screens/sign_in/SignInScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,9 +17,10 @@ export default function App() {
           // tắt header
           headerShown: false,
         }}
-        initialRouteName="SignUp">
+        initialRouteName="SignIn">
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="ClassDetail" component={ClassDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/sign_in/SignInScreen.js
+++ src/screens/sign_in/SignInScreen.js
@@ -1,35 +1,27 @@
 import { View, Text, TouchableOpacity, SafeAreaView, ImageBackground } from 'react-native'
-import React, { useRef, useState } from 'react'
 import styles from './style'
 import colors from '../../../contains/colors'
 import CustomInput from '../../components/CustomInput/CustomInput'
 import CustomButton from '../../components/CustomButton/CustomButton'
-import { Formik, } from 'formik'
+import { Formik } from 'formik'
 import { SignupSchema } from '../../../contains/validation'
+import React, { useState } from 'react'
 
-export default SignUpScreen = ({navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState();
-  const [rePassword, setRePassword] = useState();
 
+const SignInScreen = ({navigation}) => {
+  
   const [hide, setHide] = useState(true);
   const [reHide, setReHide] = useState(true);
-  const handleSubmit = () => {
-    console.log("email");
 
-  }
-  const onSignUpGoogle = () => {
-    console.log("Sign Up Google");
-  }
-  const onPressSignIn = () => {
-    console.log("Sign In");
-  }
   const changeSecureText = () => {
     if (hide == true) {
       setHide(false);
     } else {
       setHide(true);
     }
+  }
+  const onSignUpGoogle = () => {
+    console.log("Sign Up Google");
   }
   const changeReSecureText = () => {
     if (reHide == true) {
@@ -38,27 +30,26 @@ export default SignUpScreen = ({navigation }) => {
       setReHide(true);
     }
   }
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ImageBackground style={{ height: 830 }} source={require('../../../assets/images/bgSignUp.png')} resizeMode="cover" >
         {/* top */}
 
         {/* Title */}
-        <View style={{ top: 120, marginLeft: 20 }}>
+        <View style={{ top: 130, marginLeft: 20 }}>
           <View >
-            <Text style={styles.title}>Tạo tài khoản</Text>
+            <Text style={styles.title}>Đăng Nhập</Text>
           </View>
           <View style={{
             width: 271,
             height: 16,
           }}>
-            <Text style={styles.subTitle}>Chào mừng bạn đến với Flashcard Master
+            <Text style={styles.subTitle}>Vui lòng đăng nhập để tiếp tục
             </Text>
           </View>
         </View>
         {/* Form */}
-        <View style={{ top: 50 }} >
+        <View style={{ top: 90 }} >
           {/* FormText */}
           <Formik
             initialValues={{
@@ -72,25 +63,24 @@ export default SignUpScreen = ({navigation }) => {
           >
 
             {({ handleChange, handleBlur, handleSubmit, values, touched, errors, isEye }) => (
-              <View style={{ marginHorizontal: 20, top: 100 }}  >
+              <View style={{ marginHorizontal: 20, top: 120 }}  >
                 <CustomInput onChangeText={handleChange('email')} iconEye=" "
                   onBlur={handleBlur('email')} value={values.email} keyboardType="default" secureTextEntry={false} placeholder="Email" icon="envelope" errors={errors.email} touched={touched.email} isEye={false} />
                 <CustomInput onChangeText={handleChange('password')} changeIcon={hide}
                   onBlur={handleBlur('password')} onPress={changeSecureText} secureTextEntry={hide} value={values.password} keyboardType="password" placeholder="Mật khẩu" icon="lock" iconEye="eye" errors={errors.password} touched={touched.password} isEye={true} />
-                <CustomInput onChangeText={handleChange('rePassword')} changeIcon={reHide}
-                  onBlur={handleBlur('rePassword')} secureTextEntry={reHide} onPress={changeReSecureText} value={values.rePassword} keyboardType="password" placeholder="Nhập lại mật khẩu" icon="lock" iconEye="eye" errors={errors.rePassword} touched={touched.rePassword} isEye={true} />
+                
                 {/* BottomForm */}
                 <View >
-                  <CustomButton text="Đăng ký" onPress={handleSubmit} hide="hide" />
+                  <CustomButton text="Đăng nhập" onPress={handleSubmit} hide="hide" />
                   <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginVertical: 10 }}>
-                    <Text style={[styles.btnText, { color: colors.text }]}>Bạn đã có tài khoản?</Text>
-                    <TouchableOpacity activeOpacity={0.5} onPress={()=>navigation.navigate("SignIn")}>
+                    <Text style={[styles.btnText, { color: colors.text }]}>Bạn chưa có tài khoản?</Text>
+                    <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate("SignUp")}>
                       <Text style={styles.textSignIn}>
-                        Đăng nhập
+                        Đăng ký
                       </Text>
                     </TouchableOpacity>
                   </View>
-                  <CustomButton text="Đăng ký bằng Google" onPress={onSignUpGoogle} type="GG" />
+                  <CustomButton text="Đăng nhập bằng Google" onPress={onSignUpGoogle} type="GG" />
 
                 </View>
               </View>
@@ -103,3 +93,4 @@ export default SignUpScreen = ({navigation }) => {
   )
 }
 
+export default SignInScreen
