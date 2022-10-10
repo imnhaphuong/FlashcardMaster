83d8f565adfe262513bb2b097d5fbc34ebe1c4ae
LinhTrang <trangnguyen24201@gmail.com>
2022-10-10 16:47:02 +0700

    fix conflict file NavigationBar.js

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -26,14 +26,6 @@ export default function NavigationBar() {
             <View style={styles.view}>
               <Home stroke={focused ? colors.violet : colors.graySecondary} />
             </View>
-<<<<<<< HEAD
-          )
-        }}/>   
-       <Tab.Screen
-        name="class" component={ClassDetailScreen} options={{
-          tabBarShowLabel:false,
-          tabBarIcon:({focused}) => (
-=======
           ),
         }}
         listeners={({ navigation, route }) => ({
@@ -52,7 +44,6 @@ export default function NavigationBar() {
           headerShown: false,
           tabBarShowLabel: false,
           tabBarIcon: ({ focused }) => (
->>>>>>> main
             <View style={styles.view}>
               <Class stroke={focused ? colors.violet : colors.graySecondary} />
             </View>
